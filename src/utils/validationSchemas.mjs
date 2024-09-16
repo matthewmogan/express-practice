29470c3c18f5cb1eae19e67caa803da9b5c72a9a
matthewmogan <mattmogan@gmail.com>
2024-09-16 12:44:29 +0100

    added validation on USER POST and added a validation schema file to UTILS folder

---
+++ src/utils/validationSchemas.mjs
@@ -0,0 +1,22 @@
+export const createUserValidationSchema = {
+    userName: {
+        isLength: {
+            options: {
+                min: 5,
+                max: 32
+            },
+            errorMessage: "username must be 5 to 32 characters"   
+        },
+        notEmpty: {
+            errorMessage: "username cannot be empty"
+        },
+        isString: {
+            errorMessage: "USername must be a string"
+        },
+    },
+    displayName: {
+        notEmpty: {
+            errorMessage: "username cannot be empty"
+        }
+    }
+}
